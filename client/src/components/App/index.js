2bc8483498ed138ebb4ea484b39c8b162e8fe1d8
HoEddie <e52ho@uwaterloo.ca>
2022-05-18 20:05:53 -0400

    first commit

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,43 +1,12 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-} from 'react-router-dom';
+import * as React from 'react';
 
-import Home from '../Home';
-import PrivateRoute from '../Navigation/PrivateRoute.js';
+function App(){
+  return(
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
 
-
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      //
-    };
-  }
-
-  componentDidMount() {
-    //
-  }
-
-
-  componentWillUnmount() {
-    this.listener();
-  }
-
-
-  render() {
-    return (
-	  <Router>
-	    <div>
-        <PrivateRoute exact path="/" component={Home}/>
-	    </div>
-	  </Router>
-    );
-  }
 }
 
 export default App;
