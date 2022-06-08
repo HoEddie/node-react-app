eea5d2cd5b59c828a7c8a599f3c739f568cd31b9
HoEddie <e52ho@uwaterloo.ca>
2022-06-08 08:57:18 -0400

    New commit

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,70 +1,383 @@
 import * as React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import { createTheme, ThemeProvider, styled } from '@material-ui/core/styles';
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import SearchIcon from '@material-ui/icons/Search';
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from '@material-ui/core/InputAdornment';
+//import BackgroundImage from "./backgroundImage.jpg"
+
+const serverURL = "";
+
+const opacityValue = 0.9;
+
+const lightTheme = createTheme({
+  palette: {
+    type: 'light',
+    background: {
+      default: "#ffffff"
+    },
+    primary: {
+      main: '#ef9a9a',
+      light: '#ffcccb',
+      dark: '#ba6b6c',
+      background: '#eeeeee'
+    },
+    secondary: {
+      main: "#b71c1c",
+      light: '#f05545',
+      dark: '#7f0000'
+    },
+  },
+});
+
+
+const RecipePaper = styled(Paper)(({ theme }) => ({
+  opacity: 0.7,
+  backgroundColor: theme.palette.primary.background,
+  padding: 8,
+  borderRadius: 4,
+  marginTop: theme.spacing(2),
+  marginBottom: theme.spacing(2)
+}));
+
+const MainGridContainer = styled(Grid)(({ theme }) => ({
+  margin: theme.spacing(4),
+}))
 
 const App = () => {
 
-  const recipes = [
-    {
-      title: 'Fruit salad',
-      difficulty: '2',
-      ingredients: ['apple', 'banana', 'blueberries', 'raisins', 'walnuts'],
-      calories: "200",
-      instructions: "Wash fresh fruit. Slice fruit into pieces. Mix all ingredients in a bowl.",
-      recipeID: 1,
-    }, {
-      title: 'Avocado wrap',
-      difficulty: '3',
-      ingredients: ['avocado', 'spinach', 'pine nuts', 'mayo', 'apple', 'tortilla bread'],
-      calories: "400",
-      instructions: "Wash all fruits and vegetables. Slice avocadoes and apples. Mix all ingredients and wrap them in a tortilla bread.",
-      recipeID: 2
-    },
-  ];
+  const [expandedRecipesList, setExpandedRecipesList] = React.useState([]);
+  const [recipesList, setRecipesList] = React.useState([]);
+  const [editedItem, setEditedItem] = React.useState('');
 
-const [selectedRecipe, setSelectedRecipe] = React.useState('');
+  React.useEffect(() => {
+    loadRecipes();
+  }, []);
+
+
+  const handleToggleIngredients = (item) => {
+    var expandedRecipesListCopy = [...expandedRecipesList];
+    if (expandedRecipesList.includes(item.recipeID)) {
+      var index = expandedRecipesList.indexOf(item.recipeID);
+      expandedRecipesListCopy.splice(index, 1);
+      setExpandedRecipesList(expandedRecipesListCopy);
+      console.log('expandedRecipesList: ', expandedRecipesList);
+    } else {
+      expandedRecipesListCopy.push(item.recipeID);
+      setExpandedRecipesList(expandedRecipesListCopy);
+      console.log('expandedRecipesList: ', expandedRecipesList);
+    }
+  }
+
+  const handleEditInstructions = (item) => {
+    setEditedItem(item.recipeID);
+  }
+
+  const handleUpdateInstructions = (index, instructions) => {
+    console.log("ID: ", index);
+    var recipesListCopy = recipesList;
+    recipesListCopy[index].instructions = instructions;
+    setRecipesList(recipesListCopy);
+    setEditedItem('');
+  }
+
+  const loadRecipes = () => {
+    callApiLoadRecipes()
+      .then(res => {
+        console.log("callApiLoadRecipes returned: ", res)
+        var parsed = JSON.parse(res.express);
+        console.log("callApiLoadRecipes parsed: ", parsed);
+        setRecipesList(parsed);
+      })
+  }
+
+  const callApiLoadRecipes = async () => {
+    const url = serverURL + "/api/loadRecipes";
+    console.log(url);
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    console.log("User settings: ", body);
+    return body;
+  }
+
+  const handleIngredientSearch = (searchTerm) => {
+    callApiFindRecipe(searchTerm)
+      .then(res => {
+        console.log("callApiFindRecipe returned: ", res)
+        var parsed = JSON.parse(res.express);
+        console.log("callApiFindRecipe parsed: ", parsed[0])
+        setRecipesList(parsed);
+      });
+  }
+
+  const callApiFindRecipe = async (searchTerm) => {
+
+    const url = serverURL + "/api/findRecipe";
+    console.log(url);
+    console.log("SearchTerm: ", searchTerm);
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        //authorization: `Bearer ${this.state.token}`
+      },
+      body: JSON.stringify({
+        searchTerm: searchTerm
+      })
+    });
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    console.log("Found recipes: ", body);
+    return body;
+  }
 
 
   return (
-    <div>
-      <h1>
-        Recipe finder
-      </h1>
+    <ThemeProvider theme={lightTheme}>
+      <Box
+        sx={{
+          height: '100vh',
+          opacity: opacityValue,
+          overflow: "hidden",
+          //backgroundColor: lightTheme.palette.background.default,
+          //backgroundImage: `url(${BackgroundImage})`,
+          backgroundImage: `url(https://ov-research.uwaterloo.ca/salad.jpg)`,
+          backgroundSize: "cover"
+        }}
+      >
+        <MainGridContainer
+          container
+          spacing={1}
+          style={{ maxWidth: '50%' }}
+          direction="column"
+          justify="flex-start"
+          alignItems="stretch"
+        >
+          <Typography variant="h3" gutterBottom component="div">
+            Recipe Finder
+          </Typography>
+
+          <Search
+            label="Search by ingredient"
+            onSearch={handleIngredientSearch}
+          />
 
 
-      <List list={recipes} />
+          <List
+            recipesList={recipesList}
+            expandedRecipesList={expandedRecipesList}
+            onToggleIngredients={handleToggleIngredients}
+            onEditInstructions={handleEditInstructions}
+            onUpdateInstructions={handleUpdateInstructions}
+            editedItem={editedItem}
+          />
 
-    </div>
+        </MainGridContainer>
+      </Box>
+    </ThemeProvider>
   );
 }
 
+const Search = ({ label, onSearch }) => {
+  const [searchTerm, setSearchTerm] = React.useState('');
 
+  const onChangeSearchTerm = (event) => {
+    setSearchTerm(event.target.value);
+  }
 
-const List = ({list}) => {
+  const onHandleSearch = () => {
+    console.log("onHandleSearch", searchTerm)
+    onSearch(searchTerm);
+  }
+
   return (
-    <ul>
-      {list.map((item) => {
+        <TextField
+          id="search"
+          label={label}
+          value={searchTerm}
+          onChange={(event) => onChangeSearchTerm(event)}
+          variant="standard"
+          autoComplete="off"
+          color="secondary"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                  <IconButton
+                    onClick={() => onSearch(searchTerm)}
+                  >
+                    <SearchIcon />
+                  </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+  )
+};
+
+
+const List = ({ recipesList, expandedRecipesList, onToggleIngredients, onEditInstructions, onUpdateInstructions, editedItem }) => {
+  return (
+    <>
+      {recipesList.map((item, index) => {
         return (
-          <Item item={item} />
+          <Grid item>
+            <Item
+              item={item}
+              index={index}
+              expandedRecipesList={expandedRecipesList}
+              onToggleIngredients={onToggleIngredients}
+              onEditInstructions={onEditInstructions}
+              onUpdateInstructions={onUpdateInstructions}
+              editedItem={editedItem}
+            />
+          </Grid>
         );
       })}
-    </ul>
+    </>
 
   )
 }
 
-const Item = ({item}) => {
+const Item = ({ item, expandedRecipesList, onToggleIngredients, onEditInstructions, onUpdateInstructions, editedItem, index }) => {
+  const [instructions, setInstructions] = React.useState(item.instructions);
+
+  const onChangeInstructions = (event) => {
+    setInstructions(event.target.value);
+    //console.log(instructions);
+  }
+
+  const onApplyChanges = () => {
+    onUpdateInstructions(index, instructions);
+  }
+
   return (
-    <li>
-      <p> {"Title: " + item.title}</p>
-      <p> {"Difficulty: " + item.difficulty}</p>
-      <p>Ingredients: </p>
-      <ul>
-        {item.ingredients.map((ingredient) => (<li>{ingredient}</li>))}
-      </ul>
-      <p>{"Instructions: " + item.instructions}</p>
-      <p>{"Calories: " + item.calories}</p>
-    </li>
+    <RecipePaper>
+      <Typography
+        style={{ marginBottom: lightTheme.spacing(2) }}
+        variant="h5"
+        gutterTop
+        component="div"
+      >
+        {item.title}
+      </Typography>
+
+      <Typography
+        variant="h6"
+        component="div"
+        style={{ marginTop: lightTheme.spacing(1) }}
+      >
+        Difficulty: {item.difficulty}
+      </Typography>
+
+      {expandedRecipesList.includes(item.recipeID) && (
+        <>
+
+          <Typography variant="h6" component="div">
+            Ingredients:
+          </Typography>
+
+
+          <ul>
+            {item.ingredients.map((ingredient) => (
+              <li>
+                <Typography variant="body1" >
+                  {ingredient}
+                </Typography>
+              </li>
+            ))}
+          </ul>
+
+        </>
+      )}
+
+
+      <RecipeButton
+        item={item}
+        label={expandedRecipesList.includes(item.recipeID) ? 'Hide ingredients' : 'Show ingredients'}
+        onButtonClick={onToggleIngredients}
+      />
+
+
+
+      <Typography
+        variant="h6"
+        component="div"
+        style={{ marginTop: lightTheme.spacing(1) }}
+      >
+        Instructions:
+      </Typography>
+
+      {editedItem != item.recipeID ? (
+        <>
+          <Typography variant="body1" gutterBottom component="div">
+            {item.instructions}
+          </Typography>
+
+          <RecipeButton
+            item={item}
+            label={'Edit instructions'}
+            onButtonClick={onEditInstructions}
+          />
+        </>
+      ) : (
+        <>
+          <Grid container direction="column" justify="flex-start" alignItems="flex-start">
+            <Grid item>
+              <TextField
+                required
+                id='edit'
+                onChange={onChangeInstructions}
+                value={instructions}
+                style={{ width: 600, marginBottom: lightTheme.spacing(2) }}
+                multiline
+              />
+            </Grid>
+            <Grid item>
+              <RecipeButton
+                item={item}
+                label={'Save'}
+                onButtonClick={onApplyChanges}
+              />
+            </Grid>
+          </Grid>
+        </>
+      )}
+
+      <Typography
+        variant="h6"
+        gutterBottom
+        component="div"
+        style={{ marginTop: lightTheme.spacing(1) }}
+      >
+        Calories: {item.calories}
+      </Typography>
+
+    </RecipePaper>
   )
 }
+
+const RecipeButton = ({ item, label, onButtonClick }) => (
+  <Button
+    variant="contained"
+    color="secondary"
+    onClick={(event) => onButtonClick(item, event)}
+  >
+    {label}
+  </Button>
+)
 
 
 export default App;
