40b0f00302e50fe3fe9631047a0edd88415bbc90
HoEddie <e52ho@uwaterloo.ca>
2022-06-20 11:30:08 -0400

    First Commit

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,20 +1,17 @@
-import * as React from 'react';
-import Button from '@material-ui/core/Button';
+import React, { useState } from 'react';
+import { createTheme, ThemeProvider, styled, makeStyles, withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 import TextField from '@material-ui/core/TextField';
-import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
-import { createTheme, ThemeProvider, styled } from '@material-ui/core/styles';
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import SearchIcon from '@material-ui/icons/Search';
-import IconButton from "@material-ui/core/IconButton";
-import InputAdornment from '@material-ui/core/InputAdornment';
-//import BackgroundImage from "./backgroundImage.jpg"
-
-const serverURL = "";
-
-const opacityValue = 0.9;
-
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormLabel from '@material-ui/core/FormLabel';
+import Button from '@material-ui/core/Button'
 const lightTheme = createTheme({
   palette: {
     type: 'light',
@@ -35,349 +32,261 @@ const lightTheme = createTheme({
   },
 });
 
-
-const RecipePaper = styled(Paper)(({ theme }) => ({
-  opacity: 0.7,
-  backgroundColor: theme.palette.primary.background,
-  padding: 8,
-  borderRadius: 4,
-  marginTop: theme.spacing(2),
-  marginBottom: theme.spacing(2)
-}));
-
 const MainGridContainer = styled(Grid)(({ theme }) => ({
   margin: theme.spacing(4),
 }))
 
-const App = () => {
+const Review = () => {
 
-  const [expandedRecipesList, setExpandedRecipesList] = React.useState([]);
-  const [recipesList, setRecipesList] = React.useState([]);
-  const [editedItem, setEditedItem] = React.useState('');
+  const [selectedMovie, setSelectedMovie] = React.useState('');
+  const [enteredTitle, setEnteredTitle] = React.useState('');
+  const [enteredReview, setEnteredReview] = React.useState('');
+  const [selectedRating, setSelectedRating] = React.useState('');
+  const [titleValidation, setTitleValidation] = React.useState('');
+  const [reviewValidation, setReviewValidation] = React.useState('');
+  const [ratingValidation, setRatingValidation] = React.useState('');
+  const [submissionValidation, setSubmissionValidation] = React.useState('');
+  const [userState, setUserState] = React.useState(false);
+  
 
-  React.useEffect(() => {
-    loadRecipes();
-  }, []);
+  const handleSelectedMovie = (event) => {
+    setSelectedMovie(event.target.value);
+  };
 
+  const handleEnteredTitle = (event) => {
+    setEnteredTitle(event.target.value);
+  };
 
-  const handleToggleIngredients = (item) => {
-    var expandedRecipesListCopy = [...expandedRecipesList];
-    if (expandedRecipesList.includes(item.recipeID)) {
-      var index = expandedRecipesList.indexOf(item.recipeID);
-      expandedRecipesListCopy.splice(index, 1);
-      setExpandedRecipesList(expandedRecipesListCopy);
-      console.log('expandedRecipesList: ', expandedRecipesList);
+  const handleEnteredReview = (event) => {
+    setEnteredReview(event.target.value);
+  };
+
+  const handleSelectedRating = (event) => {
+    setSelectedRating(event.target.value);
+  };
+
+  function validateInputs() {
+
+    if (enteredTitle === "") {
+      setTitleValidation("Please enter your review title")
     } else {
-      expandedRecipesListCopy.push(item.recipeID);
-      setExpandedRecipesList(expandedRecipesListCopy);
-      console.log('expandedRecipesList: ', expandedRecipesList);
+      setTitleValidation("")
+
     }
-  }
 
-  const handleEditInstructions = (item) => {
-    setEditedItem(item.recipeID);
-  }
+    if (enteredReview === "") {
+      setReviewValidation("Please enter your review")
+    } else {
+      setReviewValidation("")
 
-  const handleUpdateInstructions = (index, instructions) => {
-    console.log("ID: ", index);
-    var recipesListCopy = recipesList;
-    recipesListCopy[index].instructions = instructions;
-    setRecipesList(recipesListCopy);
-    setEditedItem('');
-  }
+    }
+    if (selectedRating === "") {
+      setRatingValidation("Please select the rating")
+    } else {
+      setRatingValidation("")
 
-  const loadRecipes = () => {
-    callApiLoadRecipes()
-      .then(res => {
-        console.log("callApiLoadRecipes returned: ", res)
-        var parsed = JSON.parse(res.express);
-        console.log("callApiLoadRecipes parsed: ", parsed);
-        setRecipesList(parsed);
-      })
-  }
+    }
+    if (enteredTitle && enteredReview && selectedRating) {
+      setSubmissionValidation("Your review has been recieved")
+      const d =
+        {
+          movie: selectedMovie,
+          title: enteredTitle,
+          review: enteredReview,
+          rating: selectedRating
+        }
+      userReview.push(d)
+      
+    }
 
-  const callApiLoadRecipes = async () => {
-    const url = serverURL + "/api/loadRecipes";
-    console.log(url);
-
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      }
-    });
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    console.log("User settings: ", body);
-    return body;
-  }
-
-  const handleIngredientSearch = (searchTerm) => {
-    callApiFindRecipe(searchTerm)
-      .then(res => {
-        console.log("callApiFindRecipe returned: ", res)
-        var parsed = JSON.parse(res.express);
-        console.log("callApiFindRecipe parsed: ", parsed[0])
-        setRecipesList(parsed);
-      });
-  }
-
-  const callApiFindRecipe = async (searchTerm) => {
-
-    const url = serverURL + "/api/findRecipe";
-    console.log(url);
-    console.log("SearchTerm: ", searchTerm);
-
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        //authorization: `Bearer ${this.state.token}`
-      },
-      body: JSON.stringify({
-        searchTerm: searchTerm
-      })
-    });
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    console.log("Found recipes: ", body);
-    return body;
   }
 
 
   return (
-    <ThemeProvider theme={lightTheme}>
-      <Box
-        sx={{
-          height: '100vh',
-          opacity: opacityValue,
-          overflow: "hidden",
-          //backgroundColor: lightTheme.palette.background.default,
-          //backgroundImage: `url(${BackgroundImage})`,
-          backgroundImage: `url(https://ov-research.uwaterloo.ca/salad.jpg)`,
-          backgroundSize: "cover"
-        }}
+    <div>
+      <MainGridContainer
+        container
+        spacing={1}
+        style={{ maxWidth: '50%' }}
+        direction="column"
+        justify="flex-start"
+        alignItems="stretch"
       >
-        <MainGridContainer
-          container
-          spacing={1}
-          style={{ maxWidth: '50%' }}
-          direction="column"
-          justify="flex-start"
-          alignItems="stretch"
+        <Typography variant="h3" gutterBottom>
+          Review a Movie
+        </Typography>
+
+        <MovieSelection onSearch={handleSelectedMovie} />
+
+        <p>
+          {titleValidation}
+        </p>
+
+        <ReviewTitle onSearch={handleEnteredTitle} />
+
+        <p>
+          {reviewValidation}
+        </p>
+
+        <ReviewBody onSearch={handleEnteredReview} />
+
+        <p>
+          {ratingValidation}
+
+        </p>
+        <ReviewRating onSearch={handleSelectedRating} />
+
+        <p>
+          {submissionValidation}
+        </p>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={validateInputs}
         >
-          <Typography variant="h3" gutterBottom component="div">
-            Recipe Finder
-          </Typography>
+          Submit
+        </Button>
 
-          <Search
-            label="Search by ingredient"
-            onSearch={handleIngredientSearch}
-          />
+       
+        <Reviews />
 
+      </MainGridContainer>
+    </div>
 
-          <List
-            recipesList={recipesList}
-            expandedRecipesList={expandedRecipesList}
-            onToggleIngredients={handleToggleIngredients}
-            onEditInstructions={handleEditInstructions}
-            onUpdateInstructions={handleUpdateInstructions}
-            editedItem={editedItem}
-          />
-
-        </MainGridContainer>
-      </Box>
-    </ThemeProvider>
   );
 }
+const userReview = [
+ 
+]
 
-const Search = ({ label, onSearch }) => {
-  const [searchTerm, setSearchTerm] = React.useState('');
-
-  const onChangeSearchTerm = (event) => {
-    setSearchTerm(event.target.value);
-  }
-
-  const onHandleSearch = () => {
-    console.log("onHandleSearch", searchTerm)
-    onSearch(searchTerm);
-  }
-
+const Reviews = () => {
   return (
-        <TextField
-          id="search"
-          label={label}
-          value={searchTerm}
-          onChange={(event) => onChangeSearchTerm(event)}
-          variant="standard"
-          autoComplete="off"
-          color="secondary"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                  <IconButton
-                    onClick={() => onSearch(searchTerm)}
-                  >
-                    <SearchIcon />
-                  </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
-  )
-};
-
-
-const List = ({ recipesList, expandedRecipesList, onToggleIngredients, onEditInstructions, onUpdateInstructions, editedItem }) => {
-  return (
-    <>
-      {recipesList.map((item, index) => {
+    <ul>
+      {userReview.map(function (item) {
         return (
-          <Grid item>
-            <Item
-              item={item}
-              index={index}
-              expandedRecipesList={expandedRecipesList}
-              onToggleIngredients={onToggleIngredients}
-              onEditInstructions={onEditInstructions}
-              onUpdateInstructions={onUpdateInstructions}
-              editedItem={editedItem}
-            />
-          </Grid>
-        );
+          <li>
+          <p> {"Movie: " + item.movie}</p>
+          <p> {"Review Title: " + item.title}</p>
+          <p>{"Review: " + item.review}</p>
+          <p>{"Rating: " + item.rating}</p>
+        </li>)
       })}
-    </>
+    </ul>
 
   )
 }
 
-const Item = ({ item, expandedRecipesList, onToggleIngredients, onEditInstructions, onUpdateInstructions, editedItem, index }) => {
-  const [instructions, setInstructions] = React.useState(item.instructions);
 
-  const onChangeInstructions = (event) => {
-    setInstructions(event.target.value);
-    //console.log(instructions);
-  }
-
-  const onApplyChanges = () => {
-    onUpdateInstructions(index, instructions);
-  }
-
+const MovieSelection = (props) => {
   return (
-    <RecipePaper>
-      <Typography
-        style={{ marginBottom: lightTheme.spacing(2) }}
-        variant="h5"
-        gutterTop
-        component="div"
-      >
-        {item.title}
-      </Typography>
+    <div>
+      <FormControl fullWidth>
+        <InputLabel variant="standard" htmlFor="uncontrolled-native">
+          Select a Movie
+        </InputLabel>
+        <Select
 
-      <Typography
-        variant="h6"
-        component="div"
-        style={{ marginTop: lightTheme.spacing(1) }}
-      >
-        Difficulty: {item.difficulty}
-      </Typography>
+          labelId="movie-select"
+          id="movie-select"
+          label="Select Movie"
+          onChange={props.onSearch}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={"Morbius"}>Morbius</MenuItem>
+          <MenuItem value={"The Batman"}>The Batman</MenuItem>
+          <MenuItem value={"The God Father"}>The God Father</MenuItem>
+          <MenuItem value={"The Lego Movie"}>The Lego Movie</MenuItem>
+          <MenuItem value={"Morbius 2"}>Morbius 2</MenuItem>
 
-      {expandedRecipesList.includes(item.recipeID) && (
-        <>
+        </Select>
+      </FormControl>
+    </div>
+  )
+}
 
-          <Typography variant="h6" component="div">
-            Ingredients:
-          </Typography>
+const ReviewTitle = (props) => {
+  return (
+    <div>
+      <form>
+        <TextField
+          id="review-title"
+          label="Review Title"
+          variant="outlined"
+          fullWidth
+          onChange={props.onSearch}
+        />
 
+      </form>
+    </div>
+  )
+}
 
-          <ul>
-            {item.ingredients.map((ingredient) => (
-              <li>
-                <Typography variant="body1" >
-                  {ingredient}
-                </Typography>
-              </li>
-            ))}
-          </ul>
-
-        </>
-      )}
-
-
-      <RecipeButton
-        item={item}
-        label={expandedRecipesList.includes(item.recipeID) ? 'Hide ingredients' : 'Show ingredients'}
-        onButtonClick={onToggleIngredients}
+const ReviewBody = (props) => {
+  return (
+    <div>
+      <TextField
+        id="standard-multiline-flexible"
+        label="Enter Review"
+        multiline
+        variant="outlined"
+        maxRows={4}
+        fullWidth
+        onChange={props.onSearch}
       />
 
-
-
-      <Typography
-        variant="h6"
-        component="div"
-        style={{ marginTop: lightTheme.spacing(1) }}
-      >
-        Instructions:
-      </Typography>
-
-      {editedItem != item.recipeID ? (
-        <>
-          <Typography variant="body1" gutterBottom component="div">
-            {item.instructions}
-          </Typography>
-
-          <RecipeButton
-            item={item}
-            label={'Edit instructions'}
-            onButtonClick={onEditInstructions}
-          />
-        </>
-      ) : (
-        <>
-          <Grid container direction="column" justify="flex-start" alignItems="flex-start">
-            <Grid item>
-              <TextField
-                required
-                id='edit'
-                onChange={onChangeInstructions}
-                value={instructions}
-                style={{ width: 600, marginBottom: lightTheme.spacing(2) }}
-                multiline
-              />
-            </Grid>
-            <Grid item>
-              <RecipeButton
-                item={item}
-                label={'Save'}
-                onButtonClick={onApplyChanges}
-              />
-            </Grid>
-          </Grid>
-        </>
-      )}
-
-      <Typography
-        variant="h6"
-        gutterBottom
-        component="div"
-        style={{ marginTop: lightTheme.spacing(1) }}
-      >
-        Calories: {item.calories}
-      </Typography>
-
-    </RecipePaper>
+    </div>
   )
 }
 
-const RecipeButton = ({ item, label, onButtonClick }) => (
-  <Button
-    variant="contained"
-    color="secondary"
-    onClick={(event) => onButtonClick(item, event)}
-  >
-    {label}
-  </Button>
-)
+const ReviewRating = (props) => {
+  return (
+    <div>
+      <FormLabel id="movie-rating-radio-buttons">
+        Movie Rating (1-Lowest, 5-Highest)
+      </FormLabel>
+      <RadioGroup
+        name="Movie rating"
+        aria-label="rating"
+        row
+        onChange={props.onSearch}
+      >
+        <FormControlLabel value="1" control={<Radio />} label="1" />
+        <FormControlLabel value="2" control={<Radio />} label="2" />
+        <FormControlLabel value="3" control={<Radio />} label="3" />
+        <FormControlLabel value="4" control={<Radio />} label="4" />
+        <FormControlLabel value="5" control={<Radio />} label="5" />
+
+      </RadioGroup>
+    </div>
+  )
+}
+
+const List = (props) => {
+  return (
+    <ul>
+      {props.list.map((item) => {
+        return (
+          <Item item={item} />
+        );
+      })}
+    </ul>
+
+  )
+}
+
+const Item = (props) => {
+  return (
+    <li>
+      <p> {"Movie: " + props.item.movie}</p>
+      <p> {"Review Title: " + props.item.title}</p>
+      <p>{"Review: " + props.item.review}</p>
+      <p>{"Rating: " + props.item.rating}</p>
+    </li>
+  )
+}
 
 
-export default App;
+
+export default Review;
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
-
+import React, { useState } from 'react';
+import { createTheme, ThemeProvider, styled, makeStyles, withStyles, MuiThemeProvider } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import TextField from '@material-ui/core/TextField';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormLabel from '@material-ui/core/FormLabel';
+import Button from '@material-ui/core/Button'
 
 //Dev mode
 const serverURL = ""; //enable for dev mode
@@ -19,54 +28,6 @@ const serverURL = ""; //enable for dev mode
 //copy the number only and paste it in the serverURL in place of PORT, e.g.: const serverURL = "http://ov-research-4.uwaterloo.ca:3000";
 
 const fetch = require("node-fetch");
-
-const opacityValue = 0.9;
-
-const theme = createTheme({
-  palette: {
-    type: 'dark',
-    background: {
-      default: "#000000"
-    },
-    primary: {
-      main: "#52f1ff",
-    },
-    secondary: {
-      main: "#b552f7",
-    },
-  },
-});
-
-const styles = theme => ({
-  root: {
-    body: {
-      backgroundColor: "#000000",
-      opacity: opacityValue,
-      overflow: "hidden",
-    },
-  },
-  mainMessage: {
-    opacity: opacityValue,
-  },
-
-  mainMessageContainer: {
-    marginTop: "20vh",
-    marginLeft: theme.spacing(20),
-    [theme.breakpoints.down('xs')]: {
-      marginLeft: theme.spacing(4),
-    },
-  },
-  paper: {
-    overflow: "hidden",
-  },
-  message: {
-    opacity: opacityValue,
-    maxWidth: 250,
-    paddingBottom: theme.spacing(2),
-  },
-
-});
-
 
 class Home extends Component {
   constructor(props) {
@@ -114,41 +75,6 @@ class Home extends Component {
   render() {
     const { classes } = this.props;
 
-
-
-    const mainMessage = (
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        justify="flex-start"
-        alignItems="flex-start"
-        style={{ minHeight: '100vh' }}
-        className={classes.mainMessageContainer}
-      >
-        <Grid item>
-
-          <Typography
-            variant={"h3"}
-            className={classes.mainMessage}
-            align="flex-start"
-          >
-            {this.state.mode === 0 ? (
-              <React.Fragment>
-                Welcome to MSci245!
-              </React.Fragment>
-            ) : (
-              <React.Fragment>
-                Welcome back!
-              </React.Fragment>
-            )}
-          </Typography>
-
-        </Grid>
-      </Grid>
-    )
-
-
     return (
       <MuiThemeProvider theme={theme}>
         <div className={classes.root}>
@@ -169,4 +95,261 @@ Home.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(Home);
+const lightTheme = createTheme({
+  palette: {
+    type: 'light',
+    background: {
+      default: "#ffffff"
+    },
+    primary: {
+      main: '#ef9a9a',
+      light: '#ffcccb',
+      dark: '#ba6b6c',
+      background: '#eeeeee'
+    },
+    secondary: {
+      main: "#b71c1c",
+      light: '#f05545',
+      dark: '#7f0000'
+    },
+  },
+});
+
+const MainGridContainer = styled(Grid)(({ theme }) => ({
+  margin: theme.spacing(4),
+}))
+
+const Review = () => {
+
+  const [selectedMovie, setSelectedMovie] = React.useState('');
+  const [enteredTitle, setEnteredTitle] = React.useState('');
+  const [enteredReview, setEnteredReview] = React.useState('');
+  const [selectedRating, setSelectedRating] = React.useState('');
+  const [titleValidation, setTitleValidation] = React.useState('');
+  const [reviewValidation, setReviewValidation] = React.useState('');
+  const [ratingValidation, setRatingValidation] = React.useState('');
+  const [submissionValidation, setSubmissionValidation] = React.useState('');
+  const [userState, setUserState] = React.useState(false);
+  
+
+  const handleSelectedMovie = (event) => {
+    setSelectedMovie(event.target.value);
+  };
+
+  const handleEnteredTitle = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const handleEnteredReview = (event) => {
+    setEnteredReview(event.target.value);
+  };
+
+  const handleSelectedRating = (event) => {
+    setSelectedRating(event.target.value);
+  };
+
+  function validateInputs() {
+
+    if (enteredTitle === "") {
+      setTitleValidation("Please enter your review title")
+    } else {
+      setTitleValidation("")
+
+    }
+
+    if (enteredReview === "") {
+      setReviewValidation("Please enter your review")
+    } else {
+      setReviewValidation("")
+
+    }
+    if (selectedRating === "") {
+      setRatingValidation("Please select the rating")
+    } else {
+      setRatingValidation("")
+
+    }
+    if (enteredTitle && enteredReview && selectedRating) {
+      setSubmissionValidation("Your review has been recieved")
+      const d =
+        {
+          movie: selectedMovie,
+          title: enteredTitle,
+          review: enteredReview,
+          rating: selectedRating
+        }
+      userReview.push(d)
+      
+    }
+
+  }
+
+
+  return (
+    <div>
+      <MainGridContainer
+        container
+        spacing={1}
+        style={{ maxWidth: '50%' }}
+        direction="column"
+        justify="flex-start"
+        alignItems="stretch"
+      >
+        <Typography variant="h3" gutterBottom>
+          Review a Movie
+        </Typography>
+
+        <MovieSelection onSearch={handleSelectedMovie} />
+
+        <p>
+          {titleValidation}
+        </p>
+
+        <ReviewTitle onSearch={handleEnteredTitle} />
+
+        <p>
+          {reviewValidation}
+        </p>
+
+        <ReviewBody onSearch={handleEnteredReview} />
+
+        <p>
+          {ratingValidation}
+
+        </p>
+        <ReviewRating onSearch={handleSelectedRating} />
+
+        <p>
+          {submissionValidation}
+        </p>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={validateInputs}
+        >
+          Submit
+        </Button>
+
+       
+        <Reviews />
+
+      </MainGridContainer>
+    </div>
+
+  );
+}
+const userReview = [
+ 
+]
+
+const Reviews = () => {
+  return (
+    <ul>
+      {userReview.map(function (item) {
+        return (
+          <li>
+          <p> {"Movie: " + item.movie}</p>
+          <p> {"Review Title: " + item.title}</p>
+          <p>{"Review: " + item.review}</p>
+          <p>{"Rating: " + item.rating}</p>
+        </li>)
+      })}
+    </ul>
+
+  )
+}
+
+
+const MovieSelection = (props) => {
+  return (
+    <div>
+      <FormControl fullWidth>
+        <InputLabel variant="standard" htmlFor="uncontrolled-native">
+          Select a Movie
+        </InputLabel>
+        <Select
+
+          labelId="movie-select"
+          id="movie-select"
+          label="Select Movie"
+          onChange={props.onSearch}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={"Morbius"}>Morbius</MenuItem>
+          <MenuItem value={"The Batman"}>The Batman</MenuItem>
+          <MenuItem value={"The God Father"}>The God Father</MenuItem>
+          <MenuItem value={"The Lego Movie"}>The Lego Movie</MenuItem>
+          <MenuItem value={"Morbius 2"}>Morbius 2</MenuItem>
+
+        </Select>
+      </FormControl>
+    </div>
+  )
+}
+
+const ReviewTitle = (props) => {
+  return (
+    <div>
+      <form>
+        <TextField
+          id="review-title"
+          label="Review Title"
+          variant="outlined"
+          fullWidth
+          onChange={props.onSearch}
+        />
+
+      </form>
+    </div>
+  )
+}
+
+const ReviewBody = (props) => {
+  return (
+    <div>
+      <TextField
+        id="standard-multiline-flexible"
+        label="Enter Review"
+        multiline
+        variant="outlined"
+        maxRows={4}
+        fullWidth
+        onChange={props.onSearch}
+      />
+
+    </div>
+  )
+}
+
+const ReviewRating = (props) => {
+  return (
+    <div>
+      <FormLabel id="movie-rating-radio-buttons">
+        Movie Rating (1-Lowest, 5-Highest)
+      </FormLabel>
+      <RadioGroup
+        name="Movie rating"
+        aria-label="rating"
+        row
+        onChange={props.onSearch}
+      >
+        <FormControlLabel value="1" control={<Radio />} label="1" />
+        <FormControlLabel value="2" control={<Radio />} label="2" />
+        <FormControlLabel value="3" control={<Radio />} label="3" />
+        <FormControlLabel value="4" control={<Radio />} label="4" />
+        <FormControlLabel value="5" control={<Radio />} label="5" />
+
+      </RadioGroup>
+    </div>
+  )
+}
+
+
+
+
+
+export default Review;
+
+//export default withStyles(styles)(Home);
