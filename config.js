72ced6e97eec9519ee9a22b15f034af5d6d462a0
HoEddie <e52ho@uwaterloo.ca>
2022-07-10 18:51:15 -0400

    Commit

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 let config = {
   host    : 'ov-research-4.uwaterloo.ca',
-  user    : '',
-  password: '',
-  database: ''
+  user    : 'e52ho',
+  password: '37281527',
+  database: 'recipes'
 };
  
 module.exports = config;
